c1a254dbc52921094781b4717c65b2fd0c8c864a
klm5852 <35280058+klm5852@users.noreply.github.com>
2018-01-13 01:30:19 -0500

    Add files via upload

---
+++ SimpleEchoServer.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpleechoserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author xxani
+ */
+public class SimpleEchoServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println ("Simple Echo Server");
+        
+        try (ServerSocket serverSocket = new ServerSocket(6000))
+        {
+            System.out.println("Waiting for connection.....");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connected to client");
+            
+                try (BufferedReader br = new BufferedReader (new InputStreamReader(clientSocket.getInputStream()));
+               PrintWriter out = new PrintWriter (clientSocket.getOutputStream(), true)){
+               
+                    String inputLine;
+                          while ((inputLine = br.readLine()) != null) {
+                                System.out.println("Server: " + inputLine);
+                                out.println(inputLine);
+    }}
+                catch (IOException ex){
+                   
+                }
+        }
+        catch (IOException ex) {
+            //Handle exceptions
+        }
+        
+            
+            
+        }
+    
+}
