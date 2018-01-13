fc81ae04e0adc7bfa6ac0b69292e8f51c6f20cd2
Emily <Emily@DESKTOP-7AHUBOB.attlocal.net>
2018-01-13 01:04:33 -0600

    Updated server class with Kara's code.

--- Echo/src/Server.java
+++ Echo/src/Server.java
@@ -6,32 +6,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Server {
-        public static void main(String[] args) {
-            System.out.println("Simple Echo Server");
-
-        try (ServerSocket serverSocket = new ServerSocket(6000)){
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println ("Simple Echo Server");
+        
+        try (ServerSocket serverSocket = new ServerSocket(6000))
+        {
             System.out.println("Waiting for connection.....");
             Socket clientSocket = serverSocket.accept();
             System.out.println("Connected to client");
-                
-                try (BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-                }
-                catch (IOException e) {  
-                    e.printStackTrace();
-                }    
             
-        } catch (IOException e) {
-            e.printStackTrace();
+               try (BufferedReader br = new BufferedReader (new InputStreamReader(clientSocket.getInputStream()));
+               PrintWriter out = new PrintWriter (clientSocket.getOutputStream(), true)){
+               
+                    String inputLine;
+                        while ((inputLine = br.readLine()) != null) {
+                                System.out.println("Server: " + inputLine);
+                                out.println(inputLine);
+                        }
+                }
         }
-         
+        catch (IOException ex) {
+            //Handle exceptions
+        }  
     }
 }
