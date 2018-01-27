145ea99d775378f79a4c6904130b5c4a8a03e938
Emily <Emily@DESKTOP-7AHUBOB.attlocal.net>
2018-01-27 17:00:56 -0600

    Simplified classes, no locks yet.

--- L02/src/app.java
+++ L02/src/app.java
@@ -5,16 +5,17 @@
  */
 
 /**
- * IST 411 Spring 2018
- * @author Team 03
+ *
+ * @author Emily
  */
 public class app {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+      
+        Bank bank = new Bank(2,0);
+        bank.deposit(0, 200);
+        bank.deposit(1, 300);
+        bank.transfer(0, 1, 300);
+        bank.transfer(1, 0, 400);
+    }
 }
