c48c6f04ce3ae7083f3fe89bc48673547f27295d
Emily <Emily@DESKTOP-7AHUBOB.attlocal.net>
2018-01-28 19:13:37 -0600

    Updated with changes from Kara and Karin

--- L02/src/Bank.java
+++ L02/src/Bank.java
@@ -1,47 +1,61 @@
-
-import java.util.Arrays;
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Team 03
+ * Emily Malnor, Kara MacClean & Karin Martin
+ * 
  */
+
+import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Bank {
     
     double[] accounts;
+    private final Lock bankLock;
+    private final Condition sufficientFunds;
     
     public Bank(int n, double initialBalance){
         accounts = new double[n];
-        Arrays.fill(accounts, initialBalance);      
+        Arrays.fill(accounts, initialBalance);  
+        bankLock = new ReentrantLock();
+        sufficientFunds = bankLock.newCondition();
     }
     
+    //deposit amounts into accounts
     public void deposit(int accountNumber, double amount) 
     {
         Runnable r = () -> {
-        accounts[accountNumber] += amount;
-        System.out.println(Thread.currentThread() + "Deposit of " + amount + " into account " + accountNumber + " successful.");
-        System.out.println("Balance of account " + accountNumber + ": " + accounts[accountNumber]);
+            accounts[accountNumber] += amount;
+            System.out.println(Thread.currentThread() + "Deposit of " + amount + " into account " + accountNumber + " successful.");
+            System.out.println("Balance of account " + accountNumber + ": " + accounts[accountNumber]);
         };
+        
         Thread t = new Thread(r);
         t.start();
     }
     
-    public void transfer(int from, int to, double amount)
+    //transfer amounts between accounts
+    public void transfer(int from, int to, double amount) throws InterruptedException
     {
-        Runnable r = () -> {
-        System.out.println(Thread.currentThread() + "Transfering " + amount + " from account " + from + " to account " + to + ".");
-        accounts[from] -= amount;
-        accounts[to] += amount;
-        System.out.println("Balance of account " + from + ": " + accounts[from]);
-        System.out.println("Balance of account " + to + ": " + accounts[to]);
-        };
-        Thread t = new Thread(r);
-        t.start();
+        bankLock.lock();
+        try
+        {
+            while (accounts[from] < amount)
+            sufficientFunds.await();
+                System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            accounts[to] += amount;
+            System.out.println("Balance of account " + from + ": " + accounts[from]);
+            System.out.println("Balance of account " + to + ": " + accounts[to]);
+            sufficientFunds.signalAll();
+        }
+        finally
+        {
+            bankLock.unlock();
+        }
     }
+    
 }
+
--- L02/src/app.java
+++ L02/src/app.java
@@ -1,21 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author Emily
+ * @author Team 3
+ * Emily Malnor, Kara MacClean & Karin Martin
+ *
  */
 public class app {
     
     public static void main(String[] args) {
-      
+        
         Bank bank = new Bank(2,0);
+      
         bank.deposit(0, 200);
         bank.deposit(1, 300);
-        bank.transfer(0, 1, 300);
-        bank.transfer(1, 0, 400);
+      
+        //transfer funds into first account
+        Runnable r = () -> {
+            try
+            {
+                while (true)
+                {
+                    bank.transfer(0, 1, 300);
+                }
+            }
+            catch (InterruptedException e)
+            {
+            }            
+        };
+        
+        Thread t = new Thread(r);
+        t.start();
+        
+        //transfer funds into second account
+        Runnable ru = () -> {
+            try
+            {
+                while (true)
+                {
+                    bank.transfer(1, 0, 400);
+                }
+            }
+            catch (InterruptedException e)
+            {
+            }            
+         };
+        
+        Thread th = new Thread(ru);
+        th.start();
+      }
     }
-}
+
